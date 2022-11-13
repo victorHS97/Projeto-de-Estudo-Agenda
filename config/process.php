efb7562ed56bf3e756499c2b0011991ece517acd
Victor <victor.souza37@outlook.com>
2022-11-13 10:30:44 -0300

    finalizando o projeto

--- config/process.php
+++ config/process.php
@@ -37,6 +37,58 @@ if(!empty($data)){
         $error = $e->getMessage();
         echo "error: " . $error;
         }
+
+        // EDITANDO CONTATO
+    } else if($data["type"] === 'edit') {
+        $name = $data['name'];
+        $telefone = $data['phone'];
+        $description = $data['description'];
+        $id = $data['id'];
+
+        $query = "UPDATE contacts
+                  SET name = :name, number = :telefone, description = :description 
+                  WHERE id = :id
+        ";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":telefone", $telefone);
+        $stmt->bindParam(":description", $description);
+        $stmt->bindParam(":id", $id);
+        try {
+    
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato atualizado com sucesso!";
+        
+        }  catch(PDOException $e){
+            //erro na conexão
+        $error = $e->getMessage();
+        echo "error: " . $error;
+        }
+
+        //DELETANDO CONTATO
+    } else if($data["type"] === 'delete'){
+
+        $id = $data["id"];
+
+        $query = " DELETE FROM contacts WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        try {
+    
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato removido com sucesso!";
+        
+        }  catch(PDOException $e){
+            //erro na conexão
+        $error = $e->getMessage();
+        echo "error: " . $error;
+        }
+
     }
         // REDIRECIONA PARA A HOME
         header("location:". $BASE_URL . "../index.php");
